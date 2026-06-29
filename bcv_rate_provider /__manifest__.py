f7a5d05a1503ce05d8a39e8f5306e84795086bef
luistraspalacio <123170856+luistraspalacio@users.noreply.github.com>
2026-06-29 13:22:48 -0400

    Create  __manifest__.py

---
+++ bcv_rate_provider /__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Proveedor de Tasa BCV - Odoo 19',
+    'version': '19.0.1.0.0',
+    'summary': 'Actualización automática de la tasa de cambio basada en el BCV',
+    'category': 'Accounting',
+    'author': 'Wilmer',
+    'depends': ['account'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
